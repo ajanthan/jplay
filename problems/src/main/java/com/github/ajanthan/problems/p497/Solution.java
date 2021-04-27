9a6438e25fa98162cddfc10e07e43248793eea06
Ajanthan Balachandran <balaajanthan@gmail.com>
2021-04-27 15:03:33 -0700

    adding memoisation

--- problems/src/main/java/com/github/ajanthan/problems/p497/Solution.java
+++ problems/src/main/java/com/github/ajanthan/problems/p497/Solution.java
@@ -1,5 +1,7 @@
 package com.github.ajanthan.problems.p497;
 
+import java.util.Arrays;
+
 public class Solution {
     public int findTargetSumWaysV0(int[] nums, int target) {
         return findTargetSumWaysRecursively(nums, target, 0, 0);
@@ -15,6 +17,31 @@ public class Solution {
         }
         int positiveCount = findTargetSumWaysRecursively(nums, target, currentIndex + 1, runningSum + nums[currentIndex]);
         int negativeCount = findTargetSumWaysRecursively(nums, target, currentIndex + 1, runningSum - nums[currentIndex]);
+        return positiveCount + negativeCount;
+    }
+
+    public int findTargetSumWaysV1(int[] nums, int target) {
+        int[][] memo = new int[nums.length][2001];
+        for (int[] row : memo) {
+            Arrays.fill(row, Integer.MIN_VALUE);
+        }
+        return findTargetSumWaysRecursivelyV1(nums, target, 0, 0, memo);
+    }
+
+    private int findTargetSumWaysRecursivelyV1(int[] nums, int target, int currentIndex, int runningSum, int[][] memo) {
+        if (currentIndex == nums.length) {
+            if (target == runningSum) {
+                return 1;
+            } else {
+                return 0;
+            }
+        }
+        if (memo[currentIndex][runningSum + 1000] != Integer.MIN_VALUE) {
+            return memo[currentIndex][runningSum + 1000];
+        }
+        int positiveCount = findTargetSumWaysRecursively(nums, target, currentIndex + 1, runningSum + nums[currentIndex]);
+        int negativeCount = findTargetSumWaysRecursively(nums, target, currentIndex + 1, runningSum - nums[currentIndex]);
+        memo[currentIndex][runningSum + 1000] = positiveCount + negativeCount;
         return positiveCount + negativeCount;
     }
 }
