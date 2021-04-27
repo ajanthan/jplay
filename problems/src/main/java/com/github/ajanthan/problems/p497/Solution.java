aa5d98cd614ddd7ad60421746c1b3dd5beb58268
Ajanthan Balachandran <balaajanthan@gmail.com>
2021-04-27 14:40:38 -0700

    simplyfied

--- problems/src/main/java/com/github/ajanthan/problems/p497/Solution.java
+++ problems/src/main/java/com/github/ajanthan/problems/p497/Solution.java
@@ -2,20 +2,19 @@ package com.github.ajanthan.problems.p497;
 
 public class Solution {
     public int findTargetSumWaysV0(int[] nums, int target) {
-        return findTargetSumWaysRecursively(nums, target, -1, 0);
+        return findTargetSumWaysRecursively(nums, target, 0, 0);
     }
 
     private int findTargetSumWaysRecursively(int[] nums, int target, int currentIndex, int runningSum) {
-        currentIndex++;
-        if (currentIndex >= nums.length) {
+        if (currentIndex == nums.length) {
             if (target == runningSum) {
                 return 1;
             } else {
                 return 0;
             }
         }
-        int positiveCount = findTargetSumWaysRecursively(nums, target, currentIndex, runningSum + nums[currentIndex]);
-        int negativeCount = findTargetSumWaysRecursively(nums, target, currentIndex, runningSum - nums[currentIndex]);
+        int positiveCount = findTargetSumWaysRecursively(nums, target, currentIndex + 1, runningSum + nums[currentIndex]);
+        int negativeCount = findTargetSumWaysRecursively(nums, target, currentIndex + 1, runningSum - nums[currentIndex]);
         return positiveCount + negativeCount;
     }
 }
