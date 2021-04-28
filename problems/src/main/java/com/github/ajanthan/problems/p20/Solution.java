076a967fb08074fd962c149c980976e07a2f9094
Ajanthan Balachandran <balaajanthan@gmail.com>
2021-04-28 09:20:32 -0700

    stack based impl

---
+++ problems/src/main/java/com/github/ajanthan/problems/p20/Solution.java
@@ -0,0 +1,36 @@
+package com.github.ajanthan.problems.p20;
+
+import java.util.Stack;
+
+public class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack();
+        char c;
+        for (int i = 0; i < s.length(); i++) {
+            c = s.charAt(i);
+            switch (c) {
+                case '{':
+                case '[':
+                case '(':
+                    stack.push(c);
+                    break;
+                case '}':
+                    if (stack.isEmpty() || stack.pop() != '{') {
+                        return false;
+                    }
+                    break;
+                case ']':
+                    if (stack.isEmpty() || stack.pop() != '[') {
+                        return false;
+                    }
+                    break;
+                case ')':
+                    if (stack.isEmpty() || stack.pop() != '(') {
+                        return false;
+                    }
+                    break;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
---
+++ problems/src/test/java/com/github/ajanthan/problems/p20/SolutionTest.java
@@ -0,0 +1,34 @@
+package com.github.ajanthan.problems.p20;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class SolutionTest {
+    private Solution solution;
+    private String[] ins;
+    private boolean[] ans;
+
+    @Before
+    public void setUp() throws Exception {
+        solution = new Solution();
+        ins = new String[]{ "()",
+                            "()[]{}",
+                            "(]",
+                            "([)]",
+                            "{[]}",
+                            "(",
+                            "([)",
+                            "{]}",
+                            "{}[",
+                            "{}(" };
+        ans = new boolean[]{ true, true, false, false, true, false, false, false, false, false };
+    }
+
+    @Test
+    public void isValid() {
+        for (int i = 0; i < ins.length; i++) {
+            Assert.assertEquals(ans[i], solution.isValid(ins[i]));
+        }
+    }
+}
