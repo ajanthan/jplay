6fc5e334d9e0b51756937df3708cff2da256efb4
Ajanthan Balachandran <balaajanthan@gmail.com>
2021-05-02 16:48:38 -0700

    binary search

--- basics/src/main/java/com/github/ajanthan/searching/Searcher.java
+++ basics/src/main/java/com/github/ajanthan/searching/Searcher.java
@@ -11,7 +11,40 @@ public class Searcher {
     }
 
     public static int BinarySearch(int[] in, int x) {
+        return BinarySearchHelper(in, 0, in.length - 1, x);
+    }
 
-        return 0;
+    public static int BinarySearchHelper(int[] in, int s, int e, int x) {
+        if (s == e) {
+            if (in[s] == x) {
+                return s;
+            } else {
+                return -1;
+            }
+        }
+        int mid = s + (e - s) / 2;
+        if (x == in[mid]) {
+            return mid;
+        } else if (x < in[mid]) {
+            return BinarySearchHelper(in, s, mid - 1, x);
+        } else {
+            return BinarySearchHelper(in, mid + 1, e, x);
+        }
+    }
+
+    public static int FindFirst(int[] in, int x) {
+        int i = BinarySearchHelper(in, 0, in.length - 1, x);
+        while (i > 0 && in[i] == in[i - 1]) {
+            i--;
+        }
+        return i;
+    }
+
+    public static int FindLast(int[] in, int x) {
+        int i = BinarySearchHelper(in, 0, in.length - 1, x);
+        while (i < in.length && in[i] == in[i + 1]) {
+            i++;
+        }
+        return i;
     }
 }
--- basics/src/test/java/com/github/ajanthan/searching/SearcherTest.java
+++ basics/src/test/java/com/github/ajanthan/searching/SearcherTest.java
@@ -1,12 +1,11 @@
 package com.github.ajanthan.searching;
 
-import com.github.ajanthan.sorting.Sorter;
 import junit.framework.TestCase;
 import org.junit.Assert;
 
 public class SearcherTest extends TestCase {
-    private int[][][] ins = { { { 1, 2, 3, 4, 5, 6 }, { 4 } }, { { 1, 2, 3, 4, 5, 6, 7, 8, 9 }, { 0 } }, { { 1, 9, 5, 6, 7, 3, 4, 5, 5, 7, 8, 4 }, { 4 } } };
-    private int[][] ans = { { 3 }, { -1 }, { 6, 11 } };
+    private int[][][] ins = { { { 1, 2, 3, 4, 5, 6 }, { 4 } }, { { 1, 2, 3, 4, 5, 6, 7, 8, 9 }, { 0 } }, { { 1, 3, 4, 4, 5, 5, 5, 6, 7, 7, 8, 9 }, { 4 } } };
+    private int[][] ans = { { 3 }, { -1 }, { 2, 3 } };
 
     public void testLinearSearch() {
         for (int i = 0; i < ins.length; i++) {
@@ -17,8 +16,18 @@ public class SearcherTest extends TestCase {
 
     public void testBinarySearch() {
         for (int i = 0; i < ins.length; i++) {
-            int index = Searcher.BinarySearch(Sorter.QuickSort(ins[i][0]), ins[i][1][0]);
+            int index = Searcher.BinarySearch(ins[i][0], ins[i][1][0]);
             Assert.assertTrue(ans[i][0] == index || (ans[i].length > 1 && ans[i][1] == index));
         }
+    }
+
+    public void testFindFirst() {
+        int first = Searcher.FindFirst(ins[2][0], 5);
+        Assert.assertEquals(4, first);
+    }
+
+    public void testFindLast() {
+        int last = Searcher.FindLast(ins[2][0], 5);
+        Assert.assertEquals(6, last);
     }
 }
