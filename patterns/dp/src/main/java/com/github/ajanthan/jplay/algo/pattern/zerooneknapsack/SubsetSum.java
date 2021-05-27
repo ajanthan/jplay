43660340341d3d8b57b9f2815cbfc82ecbebd7de
Ajanthan Balachandran <balaajanthan@gmail.com>
2021-05-27 15:04:25 -0700

    bf solution

--- patterns/dp/src/main/java/com/github/ajanthan/jplay/algo/pattern/zerooneknapsack/SubsetSum.java
+++ patterns/dp/src/main/java/com/github/ajanthan/jplay/algo/pattern/zerooneknapsack/SubsetSum.java
@@ -25,4 +25,58 @@ public class SubsetSum {
         }
         return dp[nums.length - 1][sum];
     }
+
+    //t=s1+s2
+    //d=s2-s1
+    //d=s2-(t-s2)
+    //d=2s2-t
+    public int getMinimumSubsetSumDifference(int[] nums) {
+        int totalSum = 0;
+        for (int i : nums) {
+            totalSum += i;
+        }
+        return getMinimumSubsetSumDifference(nums, 0, 0, totalSum);
+    }
+
+    private int getMinimumSubsetSumDifference(int[] nums, int i, int sum, int total) {
+        if (i == nums.length - 1) {
+            return Math.abs(2 * sum - total);
+        }
+        int diff1, diff2;
+        diff1 = getMinimumSubsetSumDifference(nums, i + 1, sum, total);
+        diff2 = getMinimumSubsetSumDifference(nums, i + 1, sum + nums[i], total);
+        return Math.min(diff1, diff2);
+    }
+//
+//    public int getMinimumSubsetSumDifferenceV1(int[] nums) {
+//        int totalSum = 0;
+//        for (int i : nums) {
+//            totalSum += i;
+//        }
+//        int[][] dp = new int[nums.length][totalSum];
+//        int minDiff = Integer.MAX_VALUE;
+//        boolean[][] sum = new boolean[nums.length][totalSum];
+//        for (int i = 0; i < nums.length; i++) {
+//            if (nums[i] == 0) {
+//                sum[i][0] = true;
+//            }
+//        }
+//        sum[0][nums[0]] = true;
+//        minDiff = Math.min(minDiff, Math.abs(totalSum - nums[0]));
+//        for (int i = 1; i < nums.length; i++) {
+//            for (int j = 1; j < totalSum; j++) {
+//                if (sum[i - 1][j]) {
+//                    sum[i][j] = sum[i - 1][j];
+//                    minDiff = Math.min(minDiff, Math.abs(totalSum - j));
+//                }
+//                if (j >= nums[i]) {
+//                    if (sum[i - 1][j - nums[i]]) {
+//                        sum[i][j] = true;
+//                        minDiff = Math.min(minDiff, Math.abs(totalSum - j));
+//                    }
+//                }
+//            }
+//        }
+//        return minDiff;
+//    }
 }
--- patterns/dp/src/test/java/com/github/ajanthan/jplay/algo/pattern/zerooneknapsack/SubsetSumTest.java
+++ patterns/dp/src/test/java/com/github/ajanthan/jplay/algo/pattern/zerooneknapsack/SubsetSumTest.java
@@ -1,6 +1,5 @@
 package com.github.ajanthan.jplay.algo.pattern.zerooneknapsack;
 
-import com.github.ajanthan.jplay.algo.pattern.zerooneknapsack.SubsetSum;
 import junit.framework.TestCase;
 import org.junit.Assert;
 
@@ -15,5 +14,10 @@ public class SubsetSumTest extends TestCase {
     public void testHasSubsetWithSum() {
         Assert.assertTrue(subsetSum.hasSubsetWithSum(new int[]{ 3, 34, 4, 12, 5, 2 }, 9));
         Assert.assertFalse(subsetSum.hasSubsetWithSum(new int[]{ 3, 34, 4, 12, 5, 2 }, 30));
+    }
+
+    public void testGetMinimumSubsetSumDifference() {
+        Assert.assertEquals(1, subsetSum.getMinimumSubsetSumDifference(new int[]{ 1, 6, 11, 5 }));
+       // Assert.assertEquals(1, subsetSum.getMinimumSubsetSumDifferenceV1(new int[]{ 1, 6, 11, 5 }));
     }
 }
