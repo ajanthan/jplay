df8751270ea320590ab2c833c01a6b8d667c7937
Ajanthan Balachandran <balaajanthan@gmail.com>
2021-06-04 11:49:09 -0700

    search

--- basics/src/main/java/com/github/ajanthan/tree/DFS.java
+++ basics/src/main/java/com/github/ajanthan/tree/DFS.java
@@ -1,2 +1,39 @@
-package com.github.ajanthan.tree;public class DFS {
+package com.github.ajanthan.tree;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class DFS {
+    public List<Integer> traverse(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        traverseRecursively(root, list);
+        return list;
+    }
+
+    private void traverseRecursively(TreeNode root, List<Integer> list) {
+        if (root == null) {
+            return;
+        }
+        traverseRecursively(root.left, list);
+        list.add(root.val);
+        traverseRecursively(root.right, list);
+    }
+
+    public List<Integer> traverseIterator(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        TreeNode node = root.left;
+        while (node != null || !stack.isEmpty()) {
+            while (node != null) {
+                stack.push(node);
+                node = node.left;
+            }
+            node = stack.pop();
+            list.add(node.val);
+            node = node.right;
+        }
+        return list;
+    }
 }
--- basics/src/test/java/com/github/ajanthan/tree/DFSTest.java
+++ basics/src/test/java/com/github/ajanthan/tree/DFSTest.java
@@ -1,4 +1,40 @@
+package com.github.ajanthan.tree;
+
 import junit.framework.TestCase;
+import org.junit.Assert;
+
+import java.util.List;
+
 public class DFSTest extends TestCase {
-  
+
+    public void testTraverse() {
+        TreeNode root = TreeNode.toTreeNode(new Integer[]{ 5, 1, 4, null, null, 3, 6 });
+        /*
+         *      5
+         *     / \
+         *    /   \
+         *   1    4
+         *       / \
+         *      3   6
+         */
+        List<Integer> list = new DFS().traverse(root);
+        Assert.assertArrayEquals(new Integer[]{ 1, 5, 3, 4, 6 }, list.toArray(new Integer[0]));
+    }
+
+    public void testTraverseIterator() {
+        TreeNode root = TreeNode.toTreeNode(new Integer[]{ 5, 1, 4, null, null, 3, 6, 7, 8, 9, 10 });
+        /*
+         *      5
+         *     / \
+         *    /   \
+         *   1    4
+         *       / \
+         *      /   \
+         *     3     6
+         *    / \   / \
+         *   7  8  9   10
+         */
+        List<Integer> list = new DFS().traverseIterator(root);
+        Assert.assertArrayEquals(new Integer[]{ 1, 5, 7, 3, 8, 4, 9, 6, 10 }, list.toArray(new Integer[0]));
+    }
 }
