80a71f4679de1818aa724e9229e4b46366c9c26e
Ajanthan Balachandran <balaajanthan@gmail.com>
2021-06-07 12:25:52 -0700

    more inorder

--- basics/src/main/java/com/github/ajanthan/tree/InOrder.java
+++ basics/src/main/java/com/github/ajanthan/tree/InOrder.java
@@ -15,4 +15,46 @@ public class InOrder {
         root.right = listToBSTHelper(nums, mid + 1, end);
         return root;
     }
+
+    public void printTree(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        printTree(root.left);
+        System.out.print(" " + root.val + " ");
+        printTree(root.right);
+    }
+
+    public boolean isBinarySearchTree(TreeNode root) {
+        return isInOrder(root, Integer.MIN_VALUE) == null ? false : true;
+    }
+
+    private Integer isInOrder(TreeNode root, Integer previousValue) {
+        if (root == null) {
+            return previousValue;
+        }
+        if ((previousValue = isInOrder(root.left, previousValue)) == null) {
+            return null;
+        }
+        if (previousValue >= root.val) {
+            return null;
+        }
+        previousValue = root.val;
+        return isInOrder(root.right, previousValue);
+    }
+
+    public boolean isBinarySearchTreeRecursive(TreeNode root) {
+        return isBinarySearchTreeRecursive(root, Integer.MAX_VALUE, Integer.MIN_VALUE);
+    }
+
+    private boolean isBinarySearchTreeRecursive(TreeNode root, int max, int min) {
+        if (root == null) {
+            return true;
+        }
+        if (root.val >= max || root.val <= min) {
+            return false;
+        }
+        return isBinarySearchTreeRecursive(root.left, root.val, min)
+            && isBinarySearchTreeRecursive(root.right, max, root.val);
+    }
 }
--- basics/src/test/java/com/github/ajanthan/tree/InOrderTest.java
+++ basics/src/test/java/com/github/ajanthan/tree/InOrderTest.java
@@ -8,6 +8,7 @@ public class InOrderTest extends TestCase {
     public void testListToBST() {
         InOrder inOrder = new InOrder();
         TreeNode root = inOrder.listToBST(new Integer[]{ 1, 2, 3, 4, 5, 6, 7 });
+        inOrder.printTree(root);
         Assert.assertEquals(4, root.val);
         Assert.assertEquals(2, root.left.val);
         Assert.assertEquals(1, root.left.left.val);
@@ -15,5 +16,31 @@ public class InOrderTest extends TestCase {
         Assert.assertEquals(6, root.right.val);
         Assert.assertEquals(5, root.right.left.val);
         Assert.assertEquals(7, root.right.right.val);
+    }
+
+    public void testIsBinaryTree() {
+        InOrder inOrder = new InOrder();
+        TreeNode root0 = inOrder.listToBST(new Integer[]{ 1, 2, 3, 4, 5, 6, 7 });
+        TreeNode root1 = inOrder.listToBST(new Integer[]{ 1, 2, 3, 4, 6, 5, 7 });
+        TreeNode root2 = inOrder.listToBST(new Integer[]{ 1, 2, 3, 4, 5, 7, 7 });
+        TreeNode root3 = inOrder.listToBST(new Integer[]{ 1, 2, 3, 4, 5, 7, 6 });
+
+        Assert.assertTrue(inOrder.isBinarySearchTree(root0));
+        Assert.assertFalse(inOrder.isBinarySearchTree(root1));
+        Assert.assertFalse(inOrder.isBinarySearchTree(root2));
+        Assert.assertFalse(inOrder.isBinarySearchTree(root3));
+    }
+
+    public void testIsBinarySearchTreeRecursive() {
+        InOrder inOrder = new InOrder();
+        TreeNode root0 = inOrder.listToBST(new Integer[]{ 1, 2, 3, 4, 5, 6, 7 });
+        TreeNode root1 = inOrder.listToBST(new Integer[]{ 1, 2, 3, 4, 6, 5, 7 });
+        TreeNode root2 = inOrder.listToBST(new Integer[]{ 1, 2, 3, 4, 5, 7, 7 });
+        TreeNode root3 = inOrder.listToBST(new Integer[]{ 1, 2, 3, 4, 5, 7, 6 });
+
+        Assert.assertTrue(inOrder.isBinarySearchTreeRecursive(root0));
+        Assert.assertFalse(inOrder.isBinarySearchTreeRecursive(root1));
+        Assert.assertFalse(inOrder.isBinarySearchTreeRecursive(root2));
+        Assert.assertFalse(inOrder.isBinarySearchTreeRecursive(root3));
     }
 }
