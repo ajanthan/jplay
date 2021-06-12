e1e7618a737b7b7730e1f80b33f100705138f916
Ajanthan Balachandran <balaajanthan@gmail.com>
2021-06-11 18:39:14 -0700

    traversal impls

--- basics/src/main/java/com/github/ajanthan/tree/DFS.java
+++ basics/src/main/java/com/github/ajanthan/tree/DFS.java
@@ -5,35 +5,95 @@ import java.util.List;
 import java.util.Stack;
 
 public class DFS {
-    public List<Integer> traverse(TreeNode root) {
-        List<Integer> list = new ArrayList<>();
-        traverseRecursively(root, list);
+    public List<Integer> traverse(TreeNode root, Method method, Order order) {
+        List<Integer> list = null;
+        switch (method) {
+            case ITERATIVE:
+                list = traverseIterator(root, order);
+                break;
+            case RECURSIVE:
+                list = new ArrayList<>();
+                traverseRecursively(root, list, order);
+        }
         return list;
     }
 
-    private void traverseRecursively(TreeNode root, List<Integer> list) {
+    private void traverseRecursively(TreeNode root, List<Integer> list, Order order) {
         if (root == null) {
             return;
         }
-        traverseRecursively(root.left, list);
-        list.add(root.val);
-        traverseRecursively(root.right, list);
+        switch (order) {
+            case PRE_ORDER:
+                list.add(root.val);
+                traverseRecursively(root.left, list, order);
+                traverseRecursively(root.right, list, order);
+                break;
+            case IN_ORDER:
+                traverseRecursively(root.left, list, order);
+                list.add(root.val);
+                traverseRecursively(root.right, list, order);
+                break;
+            case POST_ORDER:
+                traverseRecursively(root.left, list, order);
+                traverseRecursively(root.right, list, order);
+                list.add(root.val);
+        }
     }
 
-    public List<Integer> traverseIterator(TreeNode root) {
+    private List<Integer> traverseIterator(TreeNode root, Order order) {
         List<Integer> list = new ArrayList<>();
         Stack<TreeNode> stack = new Stack<>();
         stack.push(root);
-        TreeNode node = root.left;
-        while (node != null || !stack.isEmpty()) {
-            while (node != null) {
-                stack.push(node);
-                node = node.left;
-            }
-            node = stack.pop();
-            list.add(node.val);
-            node = node.right;
+        TreeNode node;
+        switch (order) {
+            case PRE_ORDER:
+                while (!stack.isEmpty()) {
+                    node = stack.pop();
+                    list.add(node.val);
+                    if (node.right != null) {
+                        stack.push(node.right);
+                    }
+                    if (node.left != null) {
+                        stack.push(node.left);
+                    }
+                }
+                break;
+            case IN_ORDER:
+                node = root.left;
+                while (node != null || !stack.isEmpty()) {
+                    while (node != null) {
+                        stack.push(node);
+                        node = node.left;
+                    }
+                    node = stack.pop();
+                    list.add(node.val);
+                    node = node.right;
+                }
+                break;
+            case POST_ORDER:
+                Stack<TreeNode> stack2 = new Stack<>();
+                while (!stack.isEmpty()) {
+                    node = stack.pop();
+                    stack2.push(node);
+                    if (node.left != null) {
+                        stack.push(node.left);
+                    }
+                    if (node.right != null) {
+                        stack.push(node.right);
+                    }
+                }
+                while (!stack2.isEmpty()) {
+                    list.add(stack2.pop().val);
+                }
         }
         return list;
+    }
+
+    enum Order {
+        PRE_ORDER, IN_ORDER, POST_ORDER
+    }
+
+    enum Method {
+        ITERATIVE, RECURSIVE
     }
 }
--- basics/src/test/java/com/github/ajanthan/tree/DFSTest.java
+++ basics/src/test/java/com/github/ajanthan/tree/DFSTest.java
@@ -17,8 +17,14 @@ public class DFSTest extends TestCase {
          *       / \
          *      3   6
          */
-        List<Integer> list = new DFS().traverse(root);
-        Assert.assertArrayEquals(new Integer[]{ 1, 5, 3, 4, 6 }, list.toArray(new Integer[0]));
+        List<Integer> list1 = new DFS().traverse(root, DFS.Method.RECURSIVE, DFS.Order.IN_ORDER);
+        Assert.assertArrayEquals(new Integer[]{ 1, 5, 3, 4, 6 }, list1.toArray(new Integer[0]));
+
+        List<Integer> list2 = new DFS().traverse(root, DFS.Method.RECURSIVE, DFS.Order.PRE_ORDER);
+        Assert.assertArrayEquals(new Integer[]{ 5, 1, 4, 3, 6 }, list2.toArray(new Integer[0]));
+
+        List<Integer> list3 = new DFS().traverse(root, DFS.Method.RECURSIVE, DFS.Order.POST_ORDER);
+        Assert.assertArrayEquals(new Integer[]{ 1, 3, 6, 4, 5 }, list3.toArray(new Integer[0]));
     }
 
     public void testTraverseIterator() {
@@ -34,7 +40,13 @@ public class DFSTest extends TestCase {
          *    / \   / \
          *   7  8  9   10
          */
-        List<Integer> list = new DFS().traverseIterator(root);
-        Assert.assertArrayEquals(new Integer[]{ 1, 5, 7, 3, 8, 4, 9, 6, 10 }, list.toArray(new Integer[0]));
+        List<Integer> list1 = new DFS().traverse(root, DFS.Method.ITERATIVE, DFS.Order.IN_ORDER);
+        Assert.assertArrayEquals(new Integer[]{ 1, 5, 7, 3, 8, 4, 9, 6, 10 }, list1.toArray(new Integer[0]));
+
+        List<Integer> list2 = new DFS().traverse(root, DFS.Method.ITERATIVE, DFS.Order.PRE_ORDER);
+        Assert.assertArrayEquals(new Integer[]{ 5, 1, 4, 3, 7, 8, 6, 9, 10 }, list2.toArray(new Integer[0]));
+
+        List<Integer> list3 = new DFS().traverse(root, DFS.Method.ITERATIVE, DFS.Order.POST_ORDER);
+        Assert.assertArrayEquals(new Integer[]{ 1, 7, 8, 3, 9, 10, 6, 4, 5 }, list3.toArray(new Integer[0]));
     }
 }
